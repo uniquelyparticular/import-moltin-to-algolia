4f6513dae66bda0e30b4af2f477782958842a7ea
Adam Grohs <adamgrohs@Adams-MacBook-Air.local>
2019-02-21 19:06:35 -0600

    testing prettier

--- cmds/help.js
+++ cmds/help.js
@@ -1,23 +1,20 @@
 const menus = {
-    main: `
+  main: `
       moltin-to-algolia [command] <options>
   
       version ............ show package version
       help ............... show help menu for a command
       import ............. import moltin data into algolia
       `,
-  
-    import: `
+  import: `
       moltin-to-algolia import <options>
   
       --location, -l ..... the location to use
-      `,
+      `
 }
-  
-module.exports = (args) => {
-    const subCmd = args._[0] === 'help'
-        ? args._[1]
-        : args._[0]
-  
-    console.log(menus[subCmd] || menus.main)
+
+module.exports = args => {
+  const subCmd = args._[0] === 'help' ? args._[1] : args._[0]
+
+  console.log(menus[subCmd] || menus.main)
 }
